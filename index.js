993625ef60157167a54eee84a29409e63746cb78
Panartino <99611720+Panartino@users.noreply.github.com>
2022-02-14 16:05:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("messageCreate", (message) => {
 
 
     if (message.content == "ping") {
-        message.channel.send(message.channel.delete)
+        message.channel.send(message.delete)
     }
     
 
